ab0b532a79bc048a4857379dfe15102c5a76d42f
Prakhar Shukla <imprakharshukla@gmail.com>
2023-01-07 20:03:35 +0530

    feat: added og tags for base URL

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ import fs from "fs";
 import matter from "gray-matter";
 import {Blog} from "../components/blog";
 import {Contact} from "../components/contact";
+import Head from "next/head";
 
 
 export async function getStaticProps() {
@@ -51,9 +52,31 @@ export async function getStaticProps() {
 }
 
 export default function Home({posts, projects}) {
-
+    const URL = "https://prakharshukla.dev"
     return (
         <div className={""}>
+            <Head>
+                <title>{"Prakhar Shukla"}</title>
+                <meta name="description"
+                      content="Welcome to my portfolio! Lets make it happen."/>
+
+
+                <meta property="og:url" content={`${URL}/og_image.png`}/>
+                <meta property="og:type" content="website"/>
+                <meta property="og:title" content="Prakhar Shukla"/>
+                <meta property="og:description"
+                      content={"Welcome to my portfolio! Lets make it happen."}/>
+                <meta property="og:image" content={`${URL}/og_image.png`}/>
+
+
+                <meta name="twitter:card" content="summary_large_image"/>
+                <meta property="twitter:domain" content="prakharshukla.dev"/>
+                <meta property="twitter:url" content="https://prakharshukla.dev"/>
+                <meta name="twitter:title" content="Prakhar Shukla"/>
+                <meta name="twitter:description"
+                      content={"Welcome to my portfolio! Lets make it happen."}/>
+                <meta name="twitter:image" content={`${URL}/og_image.png`}/>
+            </Head>
             <Landing/>
             <About/>
             <Projects projects={projects}/>
